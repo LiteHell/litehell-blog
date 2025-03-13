4106bc5d22320949b2ad60ac9087d3a74d061d6c
LiteHell <litehell@litehell.info>
2025-03-13 15:00:23 +0000

    Deploying to gh-pages from  @ e32e1eff946227b5de86569aace58192c0405bd7 🚀

---
+++ _next/static/C4IygSzdus1WQT9k-jkf-/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fcategory","\u002Fcategory\u002F[category]","\u002Fpost\u002F[id]","\u002Ftag","\u002Ftag\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
