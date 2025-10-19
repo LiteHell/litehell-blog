1bffa2521990d870510e7ee857468ca8cf39cc8c
LiteHell <litehell@litehell.info>
2025-10-19 23:48:25 +0900

    fix: minor bug

--- src/blog/getPosts.ts
+++ src/blog/getPosts.ts
@@ -55,7 +55,7 @@ export default async function getPosts({
             ))!;
 
         const translated = preferredLang
-          ? metadataAndSource.lang === preferredLang
+          ? !!metadataAndSource.metadata.translated_at
           : false;
 
         return {
